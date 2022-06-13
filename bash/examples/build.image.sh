f94bedec4a58f7ea2e971656dfaf9bbfdec6c442
Wil Carmon <java@wcarmon.com>
2022-06-13 11:19:54 -0400

    golang progress

--- bash/examples/build.image.sh
+++ bash/examples/build.image.sh
@@ -16,19 +16,53 @@ set -u
 # ---------------------------------------------
 # -- Script arguments
 # ---------------------------------------------
+#TODO: document docker tag syntax
 readonly TAG_NAME=$1
 
 
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
+# NOTE: all paths relative to $PROJ_ROOT
+readonly APP_NAME=foo-service
+readonly DOCKERFILE=./Dockerfile
+readonly IMAGE_REPO_URI=ecr.us-east-1.amazonaws.com
+readonly REPOSITORY_NAME=my-github-project
+
 
 # ---------------------------------------------
 # -- Derived
 # ---------------------------------------------
+readonly DOCKER_BINARY=$(which docker)
+readonly QUALIFIED_REPOSITORY_NAME=my-org/${REPOSITORY_NAME}/${APP_NAME}
+
+
+# ---------------------------------------------
+# -- Validation
+# ---------------------------------------------
+# TODO: more here
 
 
 # ---------------------------------------------
 # -- Build
 # ---------------------------------------------
+echo;
+echo "|-- Building docker image ...";
+$DOCKER_BINARY build \
+  --file ${DOCKERFILE}\
+  --tag ${QUALIFIED_REPOSITORY_NAME}:${TAG_NAME}\
+  --tag ${QUALIFIED_REPOSITORY_NAME}:latest \
+  .
 
+echo
+echo "|-- Tagging docker image for Remote image repo ..."
+$DOCKER_BINARY tag ${QUALIFIED_REPOSITORY_NAME}:${TAG_NAME} ${IMAGE_REPO_URI}/${QUALIFIED_REPOSITORY_NAME}:${TAG_NAME}
+$DOCKER_BINARY tag ${QUALIFIED_REPOSITORY_NAME}:latest ${IMAGE_REPO_URI}/${QUALIFIED_REPOSITORY_NAME}:latest
+
+
+# ---------------------------------------------
+# -- Verify
+# ---------------------------------------------
+echo
+echo "|-- Successfully built and tagged image"
+$DOCKER_BINARY images -a | grep ${QUALIFIED_REPOSITORY_NAME}
