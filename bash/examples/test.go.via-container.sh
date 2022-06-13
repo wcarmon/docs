e125be952e5977fff1f987dd13f5728dd6d742f5
Wil Carmon <java@wcarmon.com>
2022-06-13 12:31:25 -0400

    golang progress

--- bash/examples/test.go.via-container.sh
+++ bash/examples/test.go.via-container.sh
@@ -27,6 +27,8 @@ readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
+readonly GOLANG_IMAGE=golang:1.18
+
 # Dir contains go.mod file
 readonly PROJ_ROOT=$PARENT_DIR
 
@@ -39,3 +41,13 @@ readonly PROJ_ROOT=$PARENT_DIR
 # ---------------------------------------------
 # -- Test
 # ---------------------------------------------
+echo
+echo "|-- Running tests in ${PROJ_ROOT}"
+
+$DOCKER_BINARY run \
+  --rm \
+  -it \
+  -v "$PROJ_ROOT":/usr/src/myapp \
+  --workdir /usr/src/myapp \
+  $GOLANG_IMAGE \
+  go test ./...
