e759c17ebb438b7485f71189d8c7c0751a3f5760
Wil Carmon <java@wcarmon.com>
2022-06-24 05:42:54 -0400

    bash, go, java, gradle progress

--- bash/examples/go/build.native.sh
+++ bash/examples/go/build.native.sh
@@ -2,7 +2,6 @@
 
 # ---------------------------------------------
 # -- Builds local binary via local go sdk
-# --
 # ---------------------------------------------
 
 #set -x # uncomment to debug script
@@ -10,17 +9,14 @@ set -e
 set -o pipefail
 set -u
 
-
 # ---------------------------------------------
 # -- Constants
 # ---------------------------------------------
 readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
 
-
 # ---------------------------------------------
 # -- Script arguments
 # ---------------------------------------------
-
 
 # ---------------------------------------------
 # -- Config
@@ -33,17 +29,14 @@ readonly OUTPUT_BINARY_NAME=foo-service
 # Dir contains go.mod file
 readonly PROJ_ROOT=$PARENT_DIR
 
-
 # ---------------------------------------------
 # -- Derived
 # ---------------------------------------------
 readonly OUTPUT_DIR="$PROJ_ROOT/bin"
 
-
 # ---------------------------------------------
 # -- Validate
 # ---------------------------------------------
-
 
 # ---------------------------------------------
 # -- Build
@@ -53,13 +46,12 @@ mkdir -p "$PROJ_ROOT/$OUTPUT_DIR"
 cd "$PROJ_ROOT" >/dev/null 2>&1
 
 echo "|-- Cross compiling go code in $(pwd)"
-GOOS=linux GOARCH=amd64 go build -o $OUTPUT_DIR/$OUTPUT_BINARY_NAME.linux.amd64 $CMD_PACKAGE;
-GOOS=darwin GOARCH=amd64 go build -o $OUTPUT_DIR/$OUTPUT_BINARY_NAME.macos.amd64 $CMD_PACKAGE;
-GOOS=windows GOARCH=amd64 go build -o $OUTPUT_DIR/$OUTPUT_BINARY_NAME.win.amd64.exe $CMD_PACKAGE;
-
+GOOS=linux GOARCH=amd64 go build -o "$OUTPUT_DIR/$OUTPUT_BINARY_NAME.linux.amd64" $CMD_PACKAGE
+GOOS=darwin GOARCH=amd64 go build -o "$OUTPUT_DIR/$OUTPUT_BINARY_NAME.macos.amd64" $CMD_PACKAGE
+GOOS=windows GOARCH=amd64 go build -o "$OUTPUT_DIR/$OUTPUT_BINARY_NAME.win.amd64.exe" $CMD_PACKAGE
 
 # NOTE: list architectures:
 #   go tool dist list;
 
 echo "|-- See binaries in $OUTPUT_DIR"
-ls -dhlt $OUTPUT_DIR/*
+ls -dhlt "$OUTPUT_DIR"/*
