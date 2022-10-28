36d4c8175215da9122297eac068c2ccf5d3259b0
Wil Carmon <java@wcarmon.com>
2022-10-28 14:11:19 -0400

    go, rust, bash, flutter, java, gradle, dart, python progress

--- bash/examples/go/run.jaeger.sh
+++ bash/examples/go/run.jaeger.sh
@@ -24,12 +24,6 @@ readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
-readonly ES_CONTAINER_NAME=es-for-jaeger
-
-# -- See https://hub.docker.com/_/elasticsearch/tags
-readonly ES_IMAGE="elasticsearch:7.17.7"
-#readonly ES_IMAGE="elasticsearch:8.4.3"  # not supported by jaeger yet
-
 readonly JAEGER_CONTAINER_NAME="jaeger"
 
 # -- See https://hub.docker.com/r/jaegertracing/all-in-one/tags
@@ -40,38 +34,9 @@ readonly JAEGER_IMAGE="jaegertracing/all-in-one:1.38"
 # ---------------------------------------------
 # -- Derived
 # ---------------------------------------------
+#GOTCHA: -d is cross platform, --directory is not
+readonly TEMP_DIR=$(mktemp -d)
 
-# ---------------------------------------------
-# -- Run ElasticSearch <-- very heavy & slow to start
-# ---------------------------------------------
-$DOCKER stop $ES_CONTAINER_NAME || true &>/dev/null
-$DOCKER rm --force $ES_CONTAINER_NAME || true &>/dev/null
-
-#$DOCKER run -it --rm \   <-- debugging
-$DOCKER run -d \
-  --name $ES_CONTAINER_NAME \
-  -e "discovery.type=single-node" \
-  -e "ES_JAVA_OPTS=-Xmx300m" \
-  -p 9200:9200 \
-  -p 9300:9300 \
-  $ES_IMAGE
-
-echo
-echo "|-- Waiting for ES to start"
-sleep 15
-
-#TODO: give it time to start (20 seconds)
-
-
-# ---------------------------------------------
-# -- Run Filebeat
-# ---------------------------------------------
-#TODO:
-#output.elasticsearch.index: "customname-%{[agent.version]}"
-#setup.template.name: "customname"
-#setup.template.pattern: "customname-%{[agent.version]}"
-#
-#TODO: dedupe: https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-deduplication.html
 
 # ---------------------------------------------
 # -- Run Jaeger
@@ -79,15 +44,18 @@ sleep 15
 $DOCKER stop $JAEGER_CONTAINER_NAME || true &>/dev/null
 $DOCKER rm --force $JAEGER_CONTAINER_NAME || true &>/dev/null
 
+#TODO: change user to non-root
+
 #$DOCKER run --rm -it \   <-- debugging
 
 $DOCKER run -d \
   --name $JAEGER_CONTAINER_NAME \
+  -e BADGER_DIRECTORY_KEY=/badger/key \
+  -e BADGER_DIRECTORY_VALUE=/badger/data \
+  -e BADGER_EPHEMERAL=false \
   -e COLLECTOR_OTLP_ENABLED=true \
   -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
-  -e ES_SERVER_URLS=http://localhost:9200 \
-  -e ES_USERNAME=elastic \
-  -e SPAN_STORAGE_TYPE=elasticsearch \
+  -e SPAN_STORAGE_TYPE=badger \
   -p 14250:14250 \
   -p 14268:14268 \
   -p 14269:14269 \
@@ -98,6 +66,7 @@ $DOCKER run -d \
   -p 6831:6831/udp \
   -p 6832:6832/udp \
   -p 9411:9411 \
+  -v $TEMP_DIR:/badger:rw \
   $JAEGER_IMAGE
 
 
@@ -105,11 +74,10 @@ $DOCKER run -d \
 # -- Report
 # ---------------------------------------------
 echo
-echo "|-- Started Jaeger & ElasticSearch"
+echo "|-- Started Jaeger"
 $DOCKER ps --filter="name=$JAEGER_CONTAINER_NAME"
-$DOCKER ps --filter="name=$ES_CONTAINER_NAME"
 
 echo
 echo
 echo "|-- Jaeger UI: http://localhost:16686"
-echo "|-- ElasticSearch UI: http://localhost:9200"
+echo "|-- Badger storage: $TEMP_DIR"
