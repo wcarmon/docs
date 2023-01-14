4753af81215434f0c0c451798c22f6f19a7c2b7f
Wil Carmon <java@wcarmon.com>
2023-01-13 19:43:10 -0500

    cleanup

--- bash/examples/jvm/build.native.via_docker.sh
+++ bash/examples/jvm/build.native.via_docker.sh
@@ -26,14 +26,13 @@ readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
-# See https://github.com/graalvm/container/pkgs/container/native-image
-readonly DOCKER_IMAGE_FOR_NATIVE_IMAGE="ghcr.io/graalvm/native-image:muslib-ol9-java17"
+# -- See https://github.com/graalvm/container/pkgs/container/native-image
+readonly NATIVE_IMAGE_DOCKER_IMAGE=ghcr.io/graalvm/native-image:muslib-ol9-java17-22.3.0-b2
 
-#TODO: rename
-readonly BINARY_NAME="bacon.bin"
+# Relative to $PROJECT_DIR/build/libs/
+readonly NATIVE_IMAGE_INPUT_JAR=app.uber.jar
 
-readonly JARS_DIR="${PARENT_DIR}/build/libs"
-readonly OUTPUT_DIR="${PARENT_DIR}/build/native"
+
 
 # ---------------------------------------------
 # -- Derived
@@ -51,22 +50,46 @@ cd "$PROJ_ROOT" >/dev/null 2>&1
 mkdir -p $OUTPUT_DIR;
 
 
-# TODO: how to get shell?
-#$DOCKER run --rm -it $DOCKER_IMAGE_FOR_NATIVE_IMAGE /bin/bash  <-- no bash
-#$DOCKER run --rm -it $DOCKER_IMAGE_FOR_NATIVE_IMAGE /bin/ash <-- no ash
-#$DOCKER run --rm -it $DOCKER_IMAGE_FOR_NATIVE_IMAGE
+mkdir -p ./build/native_image;
+rm -rf ./build/native_image/*;
 
-
+# NOTE: /bin/native-image
+# NOTE: working dir: /app
+#
+# NOTE: JAVA_HOME=/usr/lib64/graalvm/graalvm22-ce-java17
+#   openjdk version "17.0.5" 2022-10-18
+#   OpenJDK Runtime Environment GraalVM CE 22.3.0 (build 17.0.5+8-jvmci-22.3-b08)
+#   OpenJDK 64-Bit Server VM GraalVM CE 22.3.0 (build 17.0.5+8-jvmci-22.3-b08, mixed mode, sharing)
+#
 # See options here: https://www.graalvm.org/22.0/reference-manual/native-image/Options/#options-to-native-image-builder
-#$DOCKER run \
-#  --rm \
-#  -v "${JARS_DIR}":/jars:ro \
-#  -v "${OUTPUT_DIR}":/output:rw \
-#  $DOCKER_IMAGE_FOR_NATIVE_IMAGE -jar /jars/app.jar /output/${BINARY_NAME} --static --verbose -H:Log=registerResource
+#
+time $DOCKER run \
+  --name native-image-builder \
+  --rm \
+  -v $PROJECT_DIR/build/libs/$NATIVE_IMAGE_INPUT_JAR:/app/input.jar:ro \
+  -v $PROJECT_DIR/build/native_image:/app/output:rw \
+  $NATIVE_IMAGE_DOCKER_IMAGE \
+  --static \
+  --libc=musl \
+  -jar input.jar \
+  /app/output/app.bin
 
-docker pull alpine:3.17
+<<DEBUG
+IMAGE=ghcr.io/graalvm/native-image:muslib-ol9-java17-22.3.0-b2;
+PROJECT_DIR=/home/wcarmon/git-repos/modern-jvm/chrono-javafx-v3
+NATIVE_IMAGE_INPUT_JAR=app.uber.jar
 
-#--target ...
+docker run \
+  --entrypoint /bin/bash \
+  --rm \
+  -it \
+  -v $PROJECT_DIR/build/libs/$NATIVE_IMAGE_INPUT_JAR:/app/input.jar:ro \
+  -v $PROJECT_DIR/build/native_image:/app/output:rw \
+  $IMAGE;
+
+docker inspect $IMAGE;
+DEBUG
+
 
 # TODO: test binary on alpine (where it will run)
 #alpine:3.17
@@ -75,3 +98,5 @@ docker pull alpine:3.17
 # ---------------------------------------------
 # -- Report
 # ---------------------------------------------
+ls -hlt $PROJECT_DIR/build/libs/*.jar;
+ls -hlt $PROJECT_DIR/build/native_image/app*;
