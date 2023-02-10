28d95f6a0a6fac130d641b2da3f3ba3cdc2c0801
Wil Carmon <java@wcarmon.com>
2023-02-10 11:31:46 -0500

    cleanup

--- bash/examples/restart.jaeger.sh
+++ bash/examples/restart.jaeger.sh
@@ -24,30 +24,55 @@ readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
+# -- See https://hub.docker.com/r/jaegertracing/all-in-one/tags
+readonly JAEGER_IMAGE="jaegertracing/all-in-one:1.42"
+
+#GOTCHA: -d is cross platform, --directory is not
+#readonly TEMP_DIR=$(mktemp -d)
+
 readonly JAEGER_CONTAINER_NAME="jaeger"
 
-# -- See https://hub.docker.com/r/jaegertracing/all-in-one/tags
-#readonly JAEGER_IMAGE="jaegertracing/all-in-one:1"
-#readonly JAEGER_IMAGE="jaegertracing/all-in-one:latest"
-readonly JAEGER_IMAGE="jaegertracing/all-in-one:1.38"
-
 # ---------------------------------------------
-# -- Derived
-# ---------------------------------------------
-#GOTCHA: -d is cross platform, --directory is not
-readonly TEMP_DIR=$(mktemp -d)
-
-
-# ---------------------------------------------
-# -- Run Jaeger
+# -- Cleanup
 # ---------------------------------------------
 $DOCKER stop $JAEGER_CONTAINER_NAME || true &>/dev/null
 $DOCKER rm --force $JAEGER_CONTAINER_NAME || true &>/dev/null
 
+# ---------------------------------------------
+# -- Run
+# ---------------------------------------------
+
+# -- Ports: https://www.jaegertracing.io/docs/1.42/deployment/
+# - 14250 = collector: model.proto
+# - 14269 = admin, health check
+# - 14271 = admin, health check
+# - 16685 = query service
+# - 16686 = query service
+# - 16686 = UI
+# - 16687 = query service
+# - 4317 = collector: OpenTelemetry OTLP over grpc
+# - 4318 = collector: OpenTelemetry OTLP over http
+# - 6831 = compact thrift
+# - 6832 = binary thrift
+
 #TODO: change user to non-root
+$DOCKER run -d \
+  --name $JAEGER_CONTAINER_NAME \
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+  -e COLLECTOR_OTLP_ENABLED=true \
+  -p 14250:14250 \
+  -p 14268:14268 \
+  -p 14269:14269 \
+  -p 16686:16686 \
+  -p 4317:4317 \
+  -p 4318:4318 \
+  -p 5778:5778 \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 9411:9411 \
+  $JAEGER_IMAGE
 
-#$DOCKER run --rm -it \   <-- debugging
-
+<<'EXAMPLE_WITH_BADGER'
 $DOCKER run -d \
   --name $JAEGER_CONTAINER_NAME \
   -e BADGER_DIRECTORY_KEY=/badger/key \
@@ -68,7 +93,7 @@ $DOCKER run -d \
   -p 9411:9411 \
   -v $TEMP_DIR:/badger:rw \
   $JAEGER_IMAGE
-
+EXAMPLE_WITH_BADGER
 
 # ---------------------------------------------
 # -- Report
