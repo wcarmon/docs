846c54147fa19db6c58e58a5a4c4e84b44b05b6d
Wil Carmon <java@wcarmon.com>
2023-10-06 09:28:40 -0400

    cleanup

--- bash/examples/encryption/decrypt_file.sh
+++ bash/examples/encryption/decrypt_file.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # ---------------------------------------------
-# -- Decrypts and "de-warps" a file
+# -- Decrypts a file
 # --
 # -- See sister file at ./encrypt_file.sh
 # --
 # -- Examples:
-# -- SECRET_KEY="foo" $HOME/bin/decrypt_file.sh ./my-file.tar.gz.warp
-# -- SECRET_KEY="foo" OUTPUT_FILE=zzz.tar.gz $HOME/bin/decrypt_file.sh ./my-file.tar.gz.warp
+# -- SECRET_KEY="foo" $HOME/bin/decrypt_file.sh ./my-file.tar.gz.enc
+# -- SECRET_KEY="foo" OUTPUT_FILE=zzz.tar.gz $HOME/bin/decrypt_file.sh ./my-file.tar.gz.enc
 # --
 # -- Assumptions:
 # -- 1. openssl installed: https://linux.die.net/man/1/openssl
-# -- 2. dd installed: https://man7.org/linux/man-pages/man1/dd.1.html
 # --
 # ---------------------------------------------
 #set -x # uncomment to debug script
@@ -24,6 +23,7 @@ set -u # fail on unset var
 # -- Constants
 # ---------------------------------------------
 
+
 # ---------------------------------------------
 # -- Script arguments
 # ---------------------------------------------
@@ -35,8 +35,7 @@ set -u
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
-readonly UNWARPED_FILE_EXTENSION=".dec"
-readonly WARPED_FILE_EXTENSION=".warp"
+readonly ENCRYPTED_FILE_EXTENSION=".enc"
 
 
 # ---------------------------------------------
@@ -57,7 +56,6 @@ if [ -z "$OUTPUT_FILE" ]; then
 fi
 set -u
 
-readonly ENCRYPTED_OUTPUT_FILE="${INPUT_FILE%.*}$UNWARPED_FILE_EXTENSION"
 readonly OUTPUT_PARENT_DIR=$(readlink -f "$(dirname "${OUTPUT_FILE}")")
 
 
@@ -72,45 +70,28 @@ elif [ ! -f "$INPUT_FILE" ]; then
     echo "Error: input file must be a regular file: $INPUT_FILE"
     exit 101
 
-elif [[ ! "$INPUT_FILE" =~ $WARPED_FILE_EXTENSION$ ]]; then
-    echo "Error: INPUT_FILE must end with '$WARPED_FILE_EXTENSION'"
-    exit 102
+elif [[ ! "$INPUT_FILE" =~ $ENCRYPTED_FILE_EXTENSION$ ]]; then
+    echo "Error: INPUT_FILE must end with '$ENCRYPTED_FILE_EXTENSION'"
+    exit 110
 fi
 
 
 # ---------------------------------------------
 # -- Decrypt
 # ---------------------------------------------
-rm -vf "$ENCRYPTED_OUTPUT_FILE" || true
-#rm -vf "$OUTPUT_FILE"  # TODO: uncomment me
+rm -vf "$OUTPUT_FILE"
 
 echo
-echo "|-- Unwarping file: $(readlink -f $INPUT_FILE)"
-
-# -- Reverse all bytes
-dd if="$INPUT_FILE" \
-of="$ENCRYPTED_OUTPUT_FILE" \
-bs=1 \
-conv=swab
-
-
-echo
-echo "|-- Decrypting file: $(readlink -f $ENCRYPTED_OUTPUT_FILE)"
+echo "|-- Decrypting file: $(readlink -f $INPUT_FILE)"
 
 openssl enc \
 -d \
 -aes-256-cbc \
--in "$ENCRYPTED_OUTPUT_FILE" \
+-in "$INPUT_FILE" \
 -iter 10000 \
 -k "$SECRET_KEY" \
 -out "$OUTPUT_FILE" \
 -salt
-
-
-# ---------------------------------------------
-# -- Cleanup
-# ---------------------------------------------
-rm -f $ENCRYPTED_OUTPUT_FILE
 
 
 # ---------------------------------------------
@@ -119,8 +100,6 @@ rm -f $ENCRYPTED_OUTPUT_FILE
 echo
 echo "|-- See decrypted file: $(readlink -f $OUTPUT_FILE)"
 ls -lt $OUTPUT_PARENT_DIR | \
-grep -v $UNWARPED_FILE_EXTENSION | \
-grep -v $WARPED_FILE_EXTENSION | \
 head -2
 
 echo
