bf9504a83803228210deadf77b9686f4dc20026e
Wil Carmon <java@wcarmon.com>
2023-12-05 01:40:15 -0500

    cleanup

--- bash/examples/mssql/restart.mssql.sh
+++ bash/examples/mssql/restart.mssql.sh
@@ -19,16 +19,19 @@ set -u # fail on unset var
 # --------------------------------------------
 # -- Config
 # --------------------------------------------
-readonly CONTAINER_NAME="mssql_for_foobar"
-readonly DB_PASS="bacon4321"
-readonly DB_PORT=1433
-readonly PROJECT_DIR="$HOME/tmp/foobar"
-
-# Developer | Express | Standard
-readonly PRODUCT_ID="Developer"
-
 readonly MSSQL_IMAGE="mcr.microsoft.com/mssql/server:2022-latest"
 #readonly MSSQL_IMAGE="mcr.microsoft.com/mssql/server:2022-preview-ubuntu-22.04"
+
+readonly CONTAINER_NAME="mssql_for_foobar"
+readonly DB_PORT=1433
+readonly ADMIN_PORT=1434
+
+# -- Developer | Express | Standard
+readonly PRODUCT_ID="Developer"
+
+# -- GOTCHA: must be complex enough
+# -- https://learn.microsoft.com/en-us/sql/relational-databases/security/password-policy?view=sql-server-ver16
+readonly DB_PASS="bacon4321!"
 
 #readonly SQL_FOR_CREATE_TABLES="$HOME/tmp/foobar/mssql_create_table.sql"
 #readonly SQL_FOR_FOREIGN_KEYS="$HOME/tmp/foobar/foreign_keys.sql"
@@ -39,18 +42,15 @@ readonly MSSQL_IMAGE="mcr.microsoft.com/mssql/server:2022-latest"
 # --------------------------------------------
 readonly DB_HOSTNAME=$(hostname)
 
+
 # --------------------------------------------
 # -- Run
 # --------------------------------------------
-#cd "$PROJECT_DIR" >/dev/null 2>&1
-
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
 # -- See https://learn.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver16&pivots=cs1-bash
 # -- See https://hub.docker.com/_/microsoft-mssql-server
-
-set -x # uncomment to debug script
 
 docker run \
 --detach \
@@ -60,13 +60,12 @@ docker run \
 -e "ACCEPT_EULA=Y" \
 -e "MSSQL_PID=$PRODUCT_ID" \
 -e "MSSQL_SA_PASSWORD=$DB_PASS" \
+-p $ADMIN_PORT:1434 \
 -p $DB_PORT:1433 \
 $MSSQL_IMAGE
 
 # TODO: mount volumes
 
-#  -e POSTGRES_DB="$DB_NAME" \
-#  -e POSTGRES_USER="$DB_USER" \
 #  -v "$SQL_FOR_CREATE_TABLES":/docker-entrypoint-initdb.d/000.setup.sql:ro \
 #  -v "$SQL_FOR_FOREIGN_KEYS":/docker-entrypoint-initdb.d/010.fk.sql:ro \
 
@@ -82,11 +81,13 @@ docker logs $CONTAINER_NAME
 # --------------------------------------------
 # -- Connect
 # --------------------------------------------
-# TODO: example connection via local client
+echo ""
+echo "JDBC-url: jdbc:sqlserver://localhost:${DB_PORT}"
+echo "user=sa"
+echo "port=$DB_PORT"
 
-# user = mssql
-# user = SA
-# user = sa
+
+# TODO: example connection via local client
 
 #sudo docker exec -it sql1 "bash"
 #/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "<YourNewStrong@Passw0rd>"
