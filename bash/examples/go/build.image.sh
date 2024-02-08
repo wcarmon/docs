0f2d37e599bdd2c90c1aad858bba968c3f10b9e8
Wil Carmon <java@wcarmon.com>
2024-02-08 10:03:55 -0500

    cleanup

--- bash/examples/go/build.image.sh
+++ bash/examples/go/build.image.sh
@@ -11,58 +11,66 @@ set -e # exit on first error
 set -o pipefail
 set -u # fail on unset var
 
+
 # ---------------------------------------------
 # -- Constants
 # ---------------------------------------------
-readonly DOCKER=$(which docker)
+#readonly DOCKER=$(which docker)
+readonly DOCKER=docker
 readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
+
 
 # ---------------------------------------------
 # -- Script arguments
 # ---------------------------------------------
 # semver: https://semver.org/
 # eg. "1.2.3" or "4.5"
+set +u
 readonly SEMVER=$1
+set -u
 
 # ---------------------------------------------
 # -- Config
 # ---------------------------------------------
+# NOTE: all paths relative to $PROJ_ROOT
+
 # Dir contains Dockerfile
 readonly PROJ_ROOT="$PARENT_DIR"
 
 readonly DOCKERFILE=./Dockerfile
-
-# Unqualified image name
-# Final segment before the version
-readonly SHORT_IMAGE_NAME="foo"
 
 # Optional
 # Useful as a version suffix when image repo contains multiple variants
 # (eg. "-debian" or "-alpine")
 readonly TAG_SUFFIX="-go"
 
+# -- Everything before the version
+#
 # AWS (Elastic Container Registry):
 #   - Format: TODO
-#   - eg. "ecr.us-east-1.amazonaws.com"
-#
+#   - eg. "ecr.us-east-1.amazonaws.com", then your package name
+
 # Google Cloud (Artifact Registry):
 #   - Format: HOST-NAME/PROJECT-ID
-#   - eg. "us-east1-docker.pkg.dev/my-project-id"
+#   - eg. "us-east1-docker.pkg.dev/my-project-id", then your package name
 #   - See https://console.cloud.google.com/artifacts
-readonly IMAGE_REPO_URI="ecr.us-east-1.amazonaws.com"
-#readonly IMAGE_REPO_URI="us-east1-docker.pkg.dev/foo-bar"
 
+readonly QUALIFIED_IMAGE_NAME="todo-a"
 
-# AWS:  part after the IMAGE_REPO_URI and before the SHORT_IMAGE_NAME
-# Google cloud: gcloud artifacts repositories list
-readonly REPOSITORY_NAME="my-github-project"
+# -- Optional
+# -- Path to write git info as html
+# -- Ensure this path is in .gitignore
+readonly GIT_INFO_HTML_FILE="src/assets/git-info.html"
+
+# -- Optional
+# -- Path to write git info as property file
+# -- Ensure this path is in .gitignore
+readonly GIT_INFO_PROPERTY_FILE="src/assets/git-info.properties"
+
 
 # ---------------------------------------------
 # -- Derived
 # ---------------------------------------------
-# Everything before the version
-readonly QUALIFIED_IMAGE_NAME="${IMAGE_REPO_URI}/${REPOSITORY_NAME}/${SHORT_IMAGE_NAME}"
-
 readonly TAG_LATEST="latest${TAG_SUFFIX}"
 readonly TAG_NUMBERED="${SEMVER}${TAG_SUFFIX}"
 
@@ -76,13 +84,55 @@ if [ "$(echo $TAG_NUMBERED)" = "" ]; then
   exit 10
 fi
 
+
 # ---------------------------------------------
-# -- Build
+# -- Write git info
 # ---------------------------------------------
 cd "$PROJ_ROOT" >/dev/null 2>&1
 
 echo
-echo "|-- Latest commit: $(git log -1 --format='%H at %ci')"
+echo "|-- Latest commit: $(git log -1 --format='%H')"
+echo "|-- Latest commit ts: $(git log -1 --format='%ci')"
+
+if [ -n "$GIT_INFO_HTML_FILE" ]; then
+  SINK="$GIT_INFO_HTML_FILE"
+
+  # -- truncate
+  :> $SINK
+
+  echo '<!doctype html>
+  <html lang="en">
+  <body>
+    <ul>' >> $SINK
+
+  echo "    <li>gitHash: $(git log -1 --format='%H')</li>" >> $SINK
+  echo "    <li>buildTS: $(git log -1 --format='%ci')</li>" >> $SINK
+
+  echo '  </ul>
+  </body>
+  </html>' >> $SINK
+
+  echo "|-- Wrote git info as html to $SINK"
+fi
+
+
+if [ -n "$GIT_INFO_PROPERTY_FILE" ]; then
+  SINK="$GIT_INFO_PROPERTY_FILE"
+
+  # -- truncate
+  :> $SINK
+
+  echo "git.hash=$(git log -1 --format='%H')" >> $SINK
+  echo "git.ts=$(git log -1 --format='%ci')" >> $SINK
+
+  echo "|-- Wrote git info as property file to $SINK"
+fi
+
+
+# ---------------------------------------------
+# -- Build
+# ---------------------------------------------
+cd "$PROJ_ROOT" >/dev/null 2>&1
 
 echo
 echo "|-- Building & tagging docker image into local registry ..."
@@ -92,9 +142,51 @@ $DOCKER build \
   --tag "${QUALIFIED_IMAGE_NAME}:${TAG_NUMBERED}" \
   .
 
+
+# ---------------------------------------------
+# -- Load image to minikube
+# ---------------------------------------------
+#if command -v minikube &> /dev/null; then
+#  # -- See https://minikube.sigs.k8s.io/docs/handbook/pushing/#7-loading-directly-to-in-cluster-container-runtime
+#  minikube image load "${QUALIFIED_IMAGE_NAME}:${TAG_LATEST}"
+#  minikube image load "${QUALIFIED_IMAGE_NAME}:${TAG_NUMBERED}"
+#  echo "|-- Successfully loaded images into minikube"
+#
+#  echo
+#  echo "|-- Verify: minikube image ls | grep -i docker"
+#
+#else
+#    echo
+#    echo "|-- To load images into minikube:"
+#    echo "minikube image load ${QUALIFIED_IMAGE_NAME}:${TAG_LATEST}"
+#    echo "minikube image load ${QUALIFIED_IMAGE_NAME}:${TAG_NUMBERED}"
+#fi
+
+
 # ---------------------------------------------
 # -- Report
 # ---------------------------------------------
 echo
 echo "|-- Successfully built and tagged image"
 $DOCKER images -a | grep ${QUALIFIED_IMAGE_NAME}
+
+echo
+echo "|-- Debug using:"
+echo "$DOCKER run --rm -it $QUALIFIED_IMAGE_NAME:latest /bin/ash"
+echo
+echo "|-- Show effective configuration (inside container)"
+echo "nginx -T"
+
+
+readonly CONTAINER_NAME=my_svr
+echo
+echo "|-- Run using:"
+echo "$DOCKER stop $CONTAINER_NAME"
+echo "$DOCKER run --name $CONTAINER_NAME -d -p 13000:3000 -p 13001:3001 $QUALIFIED_IMAGE_NAME:latest"
+echo "$DOCKER ps -a | grep $CONTAINER_NAME"
+echo "$DOCKER logs $CONTAINER_NAME"
+
+
+# -- Cleanup
+# docker container prune --force
+# docker image prune --force
--- bash/examples/jvm/build.image.sh
+++ bash/examples/jvm/build.image.sh
@@ -11,11 +11,14 @@ set -e # exit on first error
 set -o pipefail
 set -u # fail on unset var
 
+
 # ---------------------------------------------
 # -- Constants
 # ---------------------------------------------
-readonly DOCKER=$(which docker)
+#readonly DOCKER=$(which docker)
+readonly DOCKER=docker
 readonly PARENT_DIR=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")/..")
+
 
 # ---------------------------------------------
 # -- Script arguments
@@ -36,28 +39,23 @@ readonly PROJ_ROOT="$PARENT_DIR"
 
 readonly DOCKERFILE=./Dockerfile
 
-# Unqualified image name
-# Final segment before the version
-readonly SHORT_IMAGE_NAME="foo"
-
 # Optional
 # Useful as a version suffix when image repo contains multiple variants
 # (eg. "-debian" or "-alpine")
 readonly TAG_SUFFIX="-java"
 
+# -- Everything before the version
+#
 # AWS (Elastic Container Registry):
 #   - Format: TODO
-#   - eg. "ecr.us-east-1.amazonaws.com"
-#
+#   - eg. "ecr.us-east-1.amazonaws.com", then your package name
+
 # Google Cloud (Artifact Registry):
 #   - Format: HOST-NAME/PROJECT-ID
-#   - eg. "us-east1-docker.pkg.dev/my-project-id"
+#   - eg. "us-east1-docker.pkg.dev/my-project-id", then your package name
 #   - See https://console.cloud.google.com/artifacts
-readonly IMAGE_REPO_URI=ecr.us-east-1.amazonaws.com
 
-# AWS:  TODO
-# Google cloud: gcloud artifacts repositories list
-readonly REPOSITORY_NAME=my-github-project
+readonly QUALIFIED_IMAGE_NAME="todo-a"
 
 # -- Optional
 # -- Path to write git info as html
@@ -74,9 +72,6 @@ readonly GIT_INFO_PROPERTY_FILE="src/git-info.properties"
 # ---------------------------------------------
 # -- Derived
 # ---------------------------------------------
-# Everything before the version
-readonly QUALIFIED_IMAGE_NAME="${IMAGE_REPO_URI}/${REPOSITORY_NAME}/${SHORT_IMAGE_NAME}"
-
 readonly TAG_LATEST="latest${TAG_SUFFIX}"
 readonly TAG_NUMBERED="${SEMVER}${TAG_SUFFIX}"
 
@@ -131,7 +126,7 @@ if [ -n "$GIT_INFO_PROPERTY_FILE" ]; then
   echo "git.hash=$(git log -1 --format='%H')" >> $SINK
   echo "git.ts=$(git log -1 --format='%ci')" >> $SINK
 
-  echo "|-- Writing git info as property file to $SINK"
+  echo "|-- Wrote git info as property file to $SINK"
 fi
 
 
@@ -148,9 +143,51 @@ $DOCKER build \
   --tag "${QUALIFIED_IMAGE_NAME}:${TAG_NUMBERED}" \
   .
 
+
+# ---------------------------------------------
+# -- Load image to minikube
+# ---------------------------------------------
+#if command -v minikube &> /dev/null; then
+#  # -- See https://minikube.sigs.k8s.io/docs/handbook/pushing/#7-loading-directly-to-in-cluster-container-runtime
+#  minikube image load "${QUALIFIED_IMAGE_NAME}:${TAG_LATEST}"
+#  minikube image load "${QUALIFIED_IMAGE_NAME}:${TAG_NUMBERED}"
+#  echo "|-- Successfully loaded images into minikube"
+#
+#  echo
+#  echo "|-- Verify: minikube image ls | grep -i docker"
+#
+#else
+#    echo
+#    echo "|-- To load images into minikube:"
+#    echo "minikube image load ${QUALIFIED_IMAGE_NAME}:${TAG_LATEST}"
+#    echo "minikube image load ${QUALIFIED_IMAGE_NAME}:${TAG_NUMBERED}"
+#fi
+
+
 # ---------------------------------------------
 # -- Report
 # ---------------------------------------------
 echo
 echo "|-- Successfully built and tagged image"
 $DOCKER images -a | grep ${QUALIFIED_IMAGE_NAME}
+
+echo
+echo "|-- Debug using:"
+echo "$DOCKER run --rm -it $QUALIFIED_IMAGE_NAME:latest /bin/ash"
+echo
+echo "|-- Show effective configuration (inside container)"
+echo "nginx -T"
+
+
+readonly CONTAINER_NAME=my_svr
+echo
+echo "|-- Run using:"
+echo "$DOCKER stop $CONTAINER_NAME"
+echo "$DOCKER run --name $CONTAINER_NAME -d -p 13000:3000 -p 13001:3001 $QUALIFIED_IMAGE_NAME:latest"
+echo "$DOCKER ps -a | grep $CONTAINER_NAME"
+echo "$DOCKER logs $CONTAINER_NAME"
+
+
+# -- Cleanup
+# docker container prune --force
+# docker image prune --force
