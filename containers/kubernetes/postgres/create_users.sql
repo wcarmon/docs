8bd44996a699064a6a94a795f6e4cf06fbc68eaa
Wil Carmon <java@wcarmon.com>
2024-04-12 17:20:08 -0400

    cleanup

--- containers/kubernetes/postgres/create_users.sql
+++ containers/kubernetes/postgres/create_users.sql
@@ -7,8 +7,12 @@
 -- ============================
 CREATE ROLE app_user WITH LOGIN PASSWORD 'password2' CONNECTION LIMIT 24;
 GRANT CONNECT ON DATABASE the_db_name TO app_user;
+
 GRANT USAGE ON SCHEMA public TO app_user;
+GRANT USAGE ON SCHEMA the_app_schema TO app_user;
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_user;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA the_app_schema TO app_user;
 
 
 -- ============================
@@ -16,5 +20,7 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_user;
 -- ============================
 CREATE ROLE backup_user WITH LOGIN PASSWORD 'password1' CONNECTION LIMIT 8;
 GRANT CONNECT ON DATABASE the_db_name TO backup_user;
+
 GRANT USAGE ON SCHEMA public TO backup_user;
+
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO backup_user;
