e34c932646de63dfb6b4c6636d3a9abb36a6be9c
Wil Carmon <java@wcarmon.com>
2024-08-30 14:10:12 -0400

    WIP

--- rust/serde/std_time_systime.rs
+++ rust/serde/std_time_systime.rs
@@ -17,7 +17,7 @@ pub mod std_system_time_epoch_millis {
                 })?
                 .as_millis() as i64
         } else {
-            // -- GOTCHA: duration_since only works for post epoch times, so we must flip
+            // -- GOTCHA: duration_since only works for post-epoch times, so we must flip
             -1 * UNIX_EPOCH
                 .duration_since(*ts)
                 .map_err(|e| {
