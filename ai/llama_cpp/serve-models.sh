9ed53ac28f123c46fea5655e84d0f195564d085e
Wil Carmon <java@wcarmon.com>
2025-12-10 15:54:56 -0500

    cleanup

--- ai/llama_cpp/serve-models.sh
+++ ai/llama_cpp/serve-models.sh
@@ -4,9 +4,9 @@
 # -- Runs the local models
 # --
 # -- Assumptions:
-# -- 1. models already installed via llama.cpp
-# -- 2. models added as providers in $HOME/.config/opencodeopencode.json
-# -- 3. TODO: more here
+# -- 1. llama.cpp installed
+# -- 2. models already installed via llama.cpp
+# -- 3. models added as providers in $HOME/.config/opencodeopencode.json
 # ---------------------------------------------
 
 #set -x # uncomment to debug script
@@ -52,19 +52,22 @@ mkdir "$HOME/llm"
 #   -hf Qwen/Qwen2.5-Coder-7B-Instruct-GGUF \
 #   -m $HOME/llm/qwen2.5-coder-7b-instruct-q4_k_m.gguf \
 #   --port 7701 \
-#   --host 127.0.0.1 &
+#   --host 127.0.0.1 \
+#   --ctx-size 2048 &
 
 llama-server \
   -hf Qwen/Qwen2.5-Coder-14B-Instruct-GGUF \
   -m $HOME/llm/qwen2.5-coder-14b-instruct-q4_k_m.gguf \
   --port 7702 \
-  --host 127.0.0.1 &
+  --host 127.0.0.1 \
+  --ctx-size 4096 &
 
 # llama-server \
 #   -hf Qwen/Qwen2.5-Coder-32B-Instruct-GGUF \
 #   -m $HOME/llm/qwen2.5-coder-32b-instruct-q4_k_m.gguf \
 #   --port 7703 \
-#   --host 127.0.0.1 &
+#   --host 127.0.0.1 \
+#   --ctx-size 4096 &
 
 
 # ------------------------------------------
@@ -72,7 +75,8 @@ llama-server \
   -hf bartowski/DeepSeek-Coder-V2-Lite-Instruct-GGUF \
   -m $HOME/llm/DeepSeek-Coder-V2-Lite-Instruct-Q4_K_M.gguf \
   --port 7801 \
-  --host 127.0.0.1 &
+  --host 127.0.0.1 \
+  --ctx-size 4096 &
 
 
 # ------------------------------------------
@@ -80,7 +84,8 @@ llama-server \
 #   -hf bartowski/starcoder2-15b-instruct-v0.1-GGUF \
 #   -m $HOME/llm/starcoder2-15b-instruct-v0.1-Q6_K.gguf \
 #   --port 7901 \
-#   --host 127.0.0.1 &
+#   --host 127.0.0.1 \
+#   --ctx-size 4096 &
 
 
 # ---------------------------------------------
